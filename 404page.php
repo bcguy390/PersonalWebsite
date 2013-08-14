c69dd5c02c98ca3ecadf0cb112e071402f710af7
Brian Chien <bcguy015745@gmail.com>
2013-08-13 21:42:54 -0700

    First Commit
    Add inital Files

---
+++ 404page.php
@@ -0,0 +1,22 @@
+<html>
+  <style>
+    .center {text-align: center; margin-left: auto; margin-right: auto; margin-bottom: auto; margin-top: auto;}
+  </style>
+  <head>
+    <link rel="stylesheet" href ="bootstrap/css/bootstrap.min.css">
+    <script src = "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script src = "bootstrap/js/bootstrap.min.js"></script>
+    <title>404 ERROR</title>
+  </head>
+<body>
+  <div class="hero-unit center">
+    <h1>Page Not Found <small><font face="Tahoma" color="red">Error 404</font></small></h1>
+    <br />
+    <p><b>HA YOUR LIFE SUCKS</b></p>
+    <p><b>Or ...</b></p>
+    <a href="/" class="btn btn-large btn-info"><i class="icon-home icon-white"></i> Take Me Home</a>
+  </div>
+</html>
+  
+    
+  
---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <link rel="stylesheet" href ="bootstrap/css/bootstrap.min.css">
+        <script src = "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+        <script src = "bootstrap/js/bootstrap.min.js"></script>
+        <title>
+            THIS IS A WEBSITE. YES
+        </title>
+    </head>
+    <body>
+    	<div class="navbar navbar-fixed-top">
+	    	<div class="navbar-inner">
+				<div class="container">
+					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</a>
+					<div class="nav-collapse">
+						<ul class="nav">
+							<li><a href="home">Home</a></li>
+							<li><a href="about">About</a></li>
+							<li><a href="contact">Contact</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+        <div class="container">
+            <div class ="hero-unit">
+                FILLER
+            </div>
+        </div>
+        <div class="container">
+            <div class = "hero">
+                testttt
+	    </div>
+        </div>
+    </body>
+
+</html>
+
